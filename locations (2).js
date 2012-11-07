985fc1335b326e7a0678a9de218e8915cf85319c
Kelly Maud <Kelly.Maud1@marist.edu>
2012-11-06 19:33:42 -0500

    new locations.js file
    this is the new locations.js file for the new story line

---
+++ locations (2).js
@@ -0,0 +1,62 @@
+//ALL THE DIFFERENT LOCATIONS
+
+
+      function location_bedroom(){
+        var msg="You have entered the parents bedroom where there is a COMFORTER. "+
+		"Enter -take- to add the comforter to your inventory.";
+        updateDisplay(msg);
+      }
+      
+      function location_front_yard(){
+        var msg = "You have entered the front yard.";
+        updateDisplay(msg);
+      }
+      
+      function location_back_yard(){
+        var msg = "You have entered the back yard.";
+        updateDisplay(msg);
+      }
+      
+      function location_kitchen(){
+        var msg = "You have entered the kitchen where there is a CHAIR. "+
+		  "Enter -take- to add the chair to your inventory.";
+        updateDisplay(msg);
+      }
+      
+      function location_bathroom(){
+        var msg = "You have entered the bathroom.";
+        updateDisplay(msg);
+      }
+      
+      function location_kids_bedroom(){
+        var msg = "You have entered the kids bedroom.";
+        updateDisplay(msg);          
+      }
+      
+      function location_mud_room(){
+        var msg = "You have entered the mudroom where there is a BROOMSTICK. "+
+		  "Enter -take- to add the broomstick to your inventory.";
+        updateDisplay(msg);          
+      }
+      
+      function location_game_room(){
+        var msg = "You have entered the game room";
+        updateDisplay(msg);          
+      }
+      
+      function location_office(){
+        var msg = "You have entered the office where there is STRING. "+
+		  "Enter -take- to put the string in your inventory.";
+        updateDisplay(msg);          
+      }
+      
+      function location_foyer(){
+        var msg = "You have entered the foyer.";
+        updateDisplay(msg);          
+      }
+      
+      function location_living_room(){
+        var msg = "You are in the living room where there is a couch and "+
+		  "this is where you are going to make the fort.";
+        updateDisplay(msg);          
+      }
