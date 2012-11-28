32194af620e69737747461b24883435e767c87d9
Kelly Maud <Kelly.Maud1@marist.edu>
2012-11-28 02:54:11 -0500

    final version of project 5
    I added prototypes, instances, scores, and a navigation matrix.

---
+++ locations (5).js
@@ -0,0 +1,131 @@
+      function location0(){
+        north_button.disabled=true;
+        south_button.disabled=false;
+        west_button.disabled=true;
+        east_button.disabled=false;
+	if(first_visit_location0 === true){
+		first_visit_location0 = false;
+		score = score + 5;
+	}
+      }
+      
+      function location1(){
+        north_button.disabled=true;
+        south_button.disabled=false;
+        west_button.disabled=false;
+        east_button.disabled=false;
+	if(first_visit_location1 === true){
+		first_visit_location1 = false;
+		score = score + 5;
+	}
+      }
+      
+      function location2(){
+        north_button.disabled=true;
+        south_button.disabled=false;
+        west_button.disabled=false;
+        east_button.disabled=true;
+	if(first_visit_location2 === true){
+		first_visit_location2 = false;
+		score = score + 5;
+	}
+      }
+      
+      function location3(){
+        north_button.disabled=false;
+        south_button.disabled=false;
+        west_button.disabled=true;
+        east_button.disabled=false;
+	if(first_visit_location3 === true){
+		first_visit_location3 = false;
+		score = score + 5;
+	}
+      }
+      
+      function location4(){
+        north_button.disabled=false;
+        south_button.disabled=false;
+        west_button.disabled=false;
+        east_button.disabled=false;
+	if(first_visit_location4 === true){
+		first_visit_location4 = false;
+		score = score + 5;
+	}         
+      }
+      
+      function location5(){
+        north_button.disabled=false;
+        south_button.disabled=false;
+        west_button.disabled=false;
+        east_button.disabled=true;
+	if(first_visit_location5 === true){
+		first_visit_location5 = false;
+		score = score + 5;
+	}
+      }
+      
+      function location6(){
+        north_button.disabled=false;
+        south_button.disabled=false;
+        west_button.disabled=true;
+        east_button.disabled=false;
+	if(first_visit_location6 === true){
+		first_visit_location6 = false;
+		score = score + 5;
+	}
+      }
+      
+      function location7(){
+        north_button.disabled=false;
+        south_button.disabled=false;
+        west_button.disabled=false;
+        east_button.disabled=false;
+	if(first_visit_location7 === true){
+		first_visit_location7 = false;
+		score = score + 5;
+	}    
+      }
+      
+      function location8(){
+        north_button.disabled=false;
+        south_button.disabled=false;
+        west_button.disabled=false;
+        east_button.disabled=true; 
+	if(first_visit_location8 === true){
+		first_visit_location8 = false;
+		score = score + 5;
+	}         
+      }
+      
+      function location9(){
+        north_button.disabled=false;
+        south_button.disabled=true;
+        west_button.disabled=true;
+        east_button.disabled=false;
+	if(first_visit_location9 === true){
+		first_visit_location9 = false;
+		score = score + 5;
+	}         
+      }
+      
+      function location10(){
+        north_button.disabled=false;
+        south_button.disabled=true;
+        west_button.disabled=false;
+        east_button.disabled=false;
+	if(first_visit_location10 === true){
+		first_visit_location10 = false;
+		score = score + 5;
+	}         
+      }
+      
+      function location11(){
+        north_button.disabled=false;
+        south_button.disabled=true;
+        west_button.disabled=false;
+        east_button.disabled=true;
+	if(first_visit_location11 === true){
+		first_visit_location11 = false;
+		score = score + 5;
+	}          
+      }
